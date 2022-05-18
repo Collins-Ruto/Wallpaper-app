ab36131175ffd52893dfc57d2a02ac17c7ef35cb
collins-ruto <collinsruto48@gmail.com>
2022-05-18 10:57:19 +0300

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-index.js;
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express();
+
+const port = process.env.PORT || 8000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
