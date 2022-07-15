8cb1da2a73a64a61db62acff81bf56e506784b5f
collins-ruto <collinsruto48@gmail.com>
2022-07-15 15:50:23 +0300

    initial commit

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate,} from 'react-router-dom'
 import axios from 'axios'
 
   const loadScript = (src) =>
